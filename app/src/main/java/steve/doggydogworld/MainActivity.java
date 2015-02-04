51ec4e2c12556ec8f6d0dc92d73aaf7eee05a7ba
Steve Mckinnon <elevated42@yahoo.com.au>
2015-02-04 16:42:01 +1000

    first commit

---
+++ app/src/main/java/steve/doggydogworld/MainActivity.java
@@ -0,0 +1,48 @@
+package steve.doggydogworld;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Dog myDog = new Dog();
+        myDog.name = "Biscuit";
+        Dog yourDog = new Dog();
+        yourDog.name = "Cookie";
+
+        Log.d("MainActivity", "My dog's name is "+myDog.name+". Your dog's name is "+yourDog.name+".");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
